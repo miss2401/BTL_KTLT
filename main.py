fbee569af889ffc19edce5de52cd452a0df6653c
master <nguyenchung2006pn@gmail.com>
2026-05-25 12:38:02 +0700

    first

---
+++ main.py
@@ -0,0 +1,8 @@
+import user as U
+answer = input("1.Dang nhap \n 2.Tao tai khoan \n 3.Thoat \n Nhập lựa chọn: ")
+match answer:
+    case 1:
+        print("Ten dang nhap: ")
+        
+    case 2:
+        new = 
---
+++ user.py
@@ -0,0 +1,7 @@
+class User:
+    def __init__(self, ten_dang_nhap, ho_ten, email, sdt = 000, cccd = 0):
+        self.ten_dang_nhap = ten_dang_nhap
+        self.ho_ten = ho_ten
+        self.sdt = sdt
+        self.email = email
+        self.cccd = cccd
