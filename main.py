97517f13013216c28a59e271a57f5a1d9f59181f
master <nguyenchung2006pn@gmail.com>
2026-05-26 17:03:08 +0700

    tao tai khoan

--- main.py
+++ main.py
@@ -4,13 +4,6 @@ match answer:
     case 1:
         pass
     case 2:
-        sdt = input("So dien thoai: ")
-        while(len(sdt) != 10 or CS.check_number(sdt)):
-            sdt = input("Nhap lai so dien thoai: ")
-        password = input("Mat khau: ")
-        while(len(password) < 8 or CS.check_password(password)):
-            print("Mat khau phai co it nhat 8 ki tu, co ket hop giua chu cai in hoa, chu so va ki tu dac biet")
-            password = input("Nhap lai mat khau: ")
-        print("Tao tai khoan thanh cong")
+        CS.Create_user
     case _:
         pass
