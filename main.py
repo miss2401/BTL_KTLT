d82f07e14972bf78468b9af72847b38f7c51f5a9
master <nguyenchung2006pn@gmail.com>
2026-05-26 16:59:54 +0700

    tao tai khoan

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
-import user as U
-answer = input("1.Dang nhap \n 2.Tao tai khoan \n 3.Thoat \n Nhập lựa chọn: ")
+import create_user as CS
+answer = int(input("1.Dang nhap \n2.Tao tai khoan \n3.Thoat \nNhập lựa chọn: "))
 match answer:
     case 1:
-        print("Ten dang nhap: ")
-        
+        pass
     case 2:
-        new = 
+        sdt = input("So dien thoai: ")
+        while(len(sdt) != 10 or CS.check_number(sdt)):
+            sdt = input("Nhap lai so dien thoai: ")
+        password = input("Mat khau: ")
+        while(len(password) < 8 or CS.check_password(password)):
+            print("Mat khau phai co it nhat 8 ki tu, co ket hop giua chu cai in hoa, chu so va ki tu dac biet")
+            password = input("Nhap lai mat khau: ")
+        print("Tao tai khoan thanh cong")
+    case _:
+        pass
--- user.py
+++
@@ -1,7 +0,0 @@
-class User:
-    def __init__(self, ten_dang_nhap, ho_ten, email, sdt = 000, cccd = 0):
-        self.ten_dang_nhap = ten_dang_nhap
-        self.ho_ten = ho_ten
-        self.sdt = sdt
-        self.email = email
-        self.cccd = cccd
