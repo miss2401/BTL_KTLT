db9ed9eb052d6e1d1e468feacee9390aec02d69a
AntiClare <59091138+AntiClare@users.noreply.github.com>
2026-06-09 08:51:42 +0700

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-import create_user as CS
-answer = int(input("1.Dang nhap \n2.Tao tai khoan \n3.Thoat \nNhập lựa chọn: "))
-match answer:
-    case 1:
-        pass
-    case 2:
-        CS.Create_user
-    case _:
-        pass
